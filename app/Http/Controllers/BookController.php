0875f7bbc8bb02a263bd6628014b2518f74a3f17
Eric Moore <emoore@thunkit.biz>
2026-01-29 13:45:42 -0600

    master: Comment the BookController Index method.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Http;
 
+/**
+ * Deals with books about the Lord of the Rings.
+ */
 class BookController extends Controller
 {
-    public function index()
+    /**
+     * Returns the Lord of the Ring book titles from the the-one-api.dev.
+     *
+     * @return JsonResponse
+     */
+    final public function index(): JsonResponse
     {
         try {
             $response = Http::get('https://the-one-api.dev/v2/book?limit=100');
-            
+
+            // Returns the names of the Lord of the Rings books from the api.
             if ($response->successful()) {
                 $books = $response->json()['docs'] ?? [];
                 $bookNames = array_map(function($book) {
@@ -21,9 +31,12 @@ class BookController extends Controller
                 
                 return response()->json($bookNames);
             }
-            
+
+            // Handles API errors.
             return response()->json(['error' => 'Failed to fetch books'], 500);
         } catch (\Exception $e) {
+
+            // Handles code errors.
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
